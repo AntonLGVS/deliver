e429534add4e0d46df0c8df2679e86f86724e1e4
Anton Naidzenau <lagavas@gmail.com>
2023-03-23 02:27:27 +0400

    [DELIVER] BE: initialized dictionary service

---
+++ dictionary/src/test/java/com/deliver/dictionary/DictionaryApplicationTests.java
@@ -0,0 +1,9 @@
+package com.deliver.dictionary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DictionaryApplicationTests {
+
+}
