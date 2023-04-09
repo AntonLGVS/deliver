97d91b0c20ecca348f6f58b5dd2e22bc6afb8fe6
Anton Naidzenau <lagavas@gmail.com>
2023-04-09 23:22:33 +0400

    [DELIVER] OPS: Created version management

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,22 @@
+dependencyResolutionManagement {
+    @Suppress("UnstableApiUsage")
+    repositories {
+        mavenCentral()
+    }
+}
+pluginManagement {
+    plugins {
+        java
+        groovy
+        id("org.springframework.boot") version "3.0.5"
+        id("io.spring.dependency-management") version "1.1.0"
+        kotlin("jvm") version "1.8.10"
+        kotlin("plugin.spring") version "1.8.10"
+        idea
+    }
+}
+
+// ROOT PROPERTIES
 rootProject.name = "deliver"
 
 // MODULES OF PROJECT
