b8f1ab06919f9167e3512d17f7284b9704396acd
Anton Naidzenau <lagavas@gmail.com>
2023-04-10 07:25:32 +0400

    [ORDER] BE: Update versions

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,8 +10,9 @@ pluginManagement {
         groovy
         id("org.springframework.boot") version "3.0.5"
         id("io.spring.dependency-management") version "1.1.0"
-        kotlin("jvm") version "1.8.10"
-        kotlin("plugin.spring") version "1.8.10"
+        kotlin("jvm") version "1.8.20"
+        kotlin("plugin.spring") version "1.8.20"
+        kotlin("kapt") version "1.8.20"
         idea
     }
 }
